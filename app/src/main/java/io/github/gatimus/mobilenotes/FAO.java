f3438e94e38d83543f72dd080dc5a57f71e74900
gatimus <william.g.lohan@gmail.com>
2015-01-22 21:59:33 -0800

    1/22 home

--- app/src/main/java/io/github/gatimus/mobilenotes/FAO.java
+++ app/src/main/java/io/github/gatimus/mobilenotes/FAO.java
@@ -1,17 +1,9 @@
 package io.github.gatimus.mobilenotes;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -60,93 +52,5 @@ public class FAO {
     } //listFiles
 
 
-    //below, from lecture
-
-    public void createFile(String fn) {
-        try {
-            OutputStreamWriter outFile = new OutputStreamWriter(openFileOutput(fn, MODE_PRIVATE));
-            outFile.write("Line 1: " + fn + '\n');
-            outFile.write("Line 2: " + fn);
-            outFile.close();
-            Toast.makeText(this, "file created: "+ fn, Toast.LENGTH_LONG).show();
-        }
-        catch(Throwable t) {
-            Toast.makeText(this, "Create Exception: "+ t.toString(), Toast.LENGTH_LONG).show();
-        }
-
-    }
-
-    public void viewFile(View v) {
-        String fileName = etFileName.getText().toString();
-        readFile(fileName);
-    }
-
-    public void readFile(String fn) {
-        try {
-            InputStream in = openFileInput(fn);
-            if(in != null)
-            {
-                InputStreamReader tmp = new InputStreamReader(in);
-                BufferedReader reader = new BufferedReader(tmp);
-                String str=null;
-                StringBuilder buf = new StringBuilder();
-                while((str = reader.readLine()) != null) {
-                    buf.append(str + "\n");
-                }
-                in.close();
-                tvFileContents.setText(buf.toString());
-            }
-            else
-                Toast.makeText(this, "Find Not Found", Toast.LENGTH_SHORT).show();
-        }
-
-
-        catch(java.io.FileNotFoundException e) {
-
-        }
-        catch(Throwable t) {
-            Toast.makeText(this, "Read Exception: " + t.toString(), Toast.LENGTH_LONG).show();
-        }
-
-    }
-
-    public void removeFile(View v) {
-        String fileName = etFileName.getText().toString();
-        deleteMyFile(fileName);
-    }
-
-    public void deleteMyFile(String fn) {
-        try {
-
-            String dir = getFilesDir().getAbsolutePath();
-            File myFile = new File(dir, fn);
-
-            boolean ret = myFile.delete();
-            if(!ret)
-                Toast.makeText(getApplicationContext(), "Delete Error",Toast.LENGTH_LONG).show();
-            else
-                Toast.makeText(getApplicationContext(), "File Deleted: " + fn,Toast.LENGTH_LONG).show();
-
-
-        }
-        catch(Throwable t) {
-            Toast.makeText(getApplicationContext(), "Delete Exception: " + t.toString(),Toast.LENGTH_LONG).show();
-        }
-
-
-    }
-
-    public void listFiles(View v) {
-
-        String[] filenames = getApplicationContext().fileList();
-
-        tvFileContents.setText("");
-        String fn="";
-        int i;
-        for(i = 0; i<filenames.length; i++){
-            fn = fn  + filenames[i] + '\n';
-        }
-        tvFileContents.setText(fn);
-    }
 
 } //class
--- app/src/main/java/io/github/gatimus/mobilenotes/Main.java
+++ app/src/main/java/io/github/gatimus/mobilenotes/Main.java
@@ -2,6 +2,8 @@ package io.github.gatimus.mobilenotes;
 
 import android.app.DialogFragment;
 import android.app.FragmentManager;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -9,6 +11,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
 
 
 public class Main extends ActionBarActivity {
@@ -18,6 +25,9 @@ public class Main extends ActionBarActivity {
     private FragmentManager fragmentManager;
     private DialogFragment about;
     private DialogFragment help;
+    private ListView listView;
+    private ArrayAdapter<String> fruitAdapter;
+    private ArrayList<String> fruit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +38,42 @@ public class Main extends ActionBarActivity {
         fragmentManager = this.getFragmentManager();
         about = new About();
         help = new Help();
+        fruit = new ArrayList<String>();
+        fruit.add("banana");
+        fruit.add("apple");
+        fruit.add("orange");
+        fruit.add("tomato");
+        fruitAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fruit);
+        fruitAdapter.setNotifyOnChange(true);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(fruitAdapter);
+        handleIntent(getIntent());
     } //onCreate
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            fruitAdapter.getFilter().filter(query);
+        }
+    }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         Log.v(TAG, "CreateOptionsMenu");
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        if(searchView != null){
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setIconifiedByDefault(false);
+        }
         return true;
     } //onCreateOptionsMenu
 
@@ -44,6 +82,11 @@ public class Main extends ActionBarActivity {
         Log.i(TAG, item.getTitle().toString() + " Selected");
         int id = item.getItemId();
         switch(id){
+            case R.id.action_search :
+                if(item.getActionView() == null){
+                    onSearchRequested();
+                }
+                break;
             case R.id.action_settings :
                 Intent intent = new Intent(Main.this, Settings.class);
                 startActivity(intent);
